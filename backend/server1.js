47d46604179dd9e83e8ecf894d43d802e3b1fe83
Bipanchi <bipanchideka14@gmail.com>
2026-09-13 13:07:58 +0530

    Fix login page and static files

--- backend/server1.js
+++ backend/server1.js
@@ -13,6 +13,10 @@ const subjectRoutes = require("./routes/subjectroutes");
 
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "..")));
 const PORT = process.env.PORT || 5000;
 
 // ===============================
